dc0a821662c6e58a1cd5573640600649753c4ce0
KieuSonTung <sontungkieu412@gmail.com>
2024-06-26 19:04:34 +0700

    feat: add preprocess_pipeline

--- src/preprocess/__init__.py
+++ src/preprocess/__init__.py
@@ -3,6 +3,7 @@ from src.preprocess.clean_categorical import convert_edu, convert_marital_status
 from src.preprocess.fill_nan import fill_missing_categorical
 from src.preprocess.encode import label_encode_datasets
 from src.preprocess.strip import remove_spaces
+from src.preprocess.pipeline import preprocess_pipeline
 
 
 __all__ = [
@@ -12,5 +13,6 @@ __all__ = [
     'convert_marital_status',
     'fill_missing_categorical',
     'label_encode_datasets',
-    'remove_spaces'
+    'remove_spaces',
+    'preprocess_pipeline'
 ]
--- src/preprocess/encode.py
+++ src/preprocess/encode.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+import joblib
+import os
 
 
-def label_encode_datasets(train_df, test_df):
+def label_encode_datasets(train_df):
     """
     Perform label encoding on categorical features in both train and test datasets.
     
     Parameters:
     train_df (pd.DataFrame): Training dataset
-    test_df (pd.DataFrame): Testing dataset
     
     Returns:
     pd.DataFrame, pd.DataFrame: Label encoded train and test datasets
@@ -16,19 +17,25 @@ def label_encode_datasets(train_df, test_df):
     # Initialize the label encoder
     label_encoders = {}
     
-    # Concatenate train and test data to ensure consistency in encoding
-    combined_df = pd.concat([train_df, test_df], axis=0)
-    
+    # Create a copy of the data to avoid modifying the original dataframes
+    train_data_encoded = train_df.copy()
+
     # Identify categorical columns
-    categorical_cols = combined_df.select_dtypes(include=['object']).columns
+    categorical_cols = train_df.select_dtypes(include=['object']).columns
     
     # Label encode each categorical column
-    for col in categorical_cols:
-        label_encoders[col] = LabelEncoder()
-        combined_df[col] = label_encoders[col].fit_transform(combined_df[col])
+    for column in categorical_cols:
+        le = LabelEncoder()
+        
+         # Fit the label encoder on the training data
+        train_data_encoded[column] = le.fit_transform(train_df[column])
+
+        # Store the label encoder
+        label_encoders[column] = le
     
-    # Split the combined dataframe back into train and test sets
-    train_encoded = combined_df.iloc[:len(train_df), :].reset_index(drop=True)
-    test_encoded = combined_df.iloc[len(train_df):, :].reset_index(drop=True)
-    
-    return train_encoded, test_encoded
+    if not os.path.exists('../weights/label_encoders'):
+        os.mkdir('../weights/label_encoders')
+            
+    joblib.dump(label_encoders, f'../weights/label_encoders/label_encoders.pkl')
+
+    return train_data_encoded
